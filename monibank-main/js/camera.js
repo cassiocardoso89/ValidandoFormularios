ec8cc4dd1c184a76718d5e4c05afcef4c5bc79e7
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-03-02 15:48:42 -0300

    salvandos fotos e dados

--- monibank-main/js/camera.js
+++ monibank-main/js/camera.js
@@ -4,20 +4,31 @@ const video = document.querySelector('[data-video]');
 const botaoTirarFoto = document.querySelector('[data-tirar-foto]');
 const canvas = document.querySelector('[data-video-canvas]');
 const mensagem = document.querySelector('[data-mensagem]');
+const botaoEnviarFoto = document.querySelector('[data-enviar]');
 
-let imagemUrl = "";
+let imagemUrl = '';
 
-botaoIniciarCamera.addEventListener("click", async function(){
+botaoIniciarCamera.addEventListener('click', async function(){
     const iniciarVideo = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
 
-    botaoIniciarCamera.style.display = "none";
-    campoCamera.style.display = "block";
+    botaoIniciarCamera.style.display = 'none';
+    campoCamera.style.display = 'block';
     video.srcObject = iniciarVideo;
 })
 
-botaoTirarFoto.addEventListener("click", function(){
+botaoTirarFoto.addEventListener('click', function(){
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-    imagemUrl = canvas.toDataURL("image/jpeg");
-    campoCamera.style.display = 'none';yy
+    imagemUrl = canvas.toDataURL('image/jpeg');
+    campoCamera.style.display = 'none';
     mensagem.style.display = 'block';
+})
+
+botaoEnviarFoto.addEventListener('click', () => {
+    const receberDadosExistentes = localStorage.getItem('cadastro');
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemUrl;
+
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno));
+    window.location.href = '../pages/abrir-conta-form-3.html';
 })
