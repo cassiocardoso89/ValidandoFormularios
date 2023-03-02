2184dc22fb455b3d8eb99e8a8f2e6d91bc9e3fe6
Cassio de Souza Cardoso <cassio.cardoso@sequor.com.br>
2023-03-02 15:04:09 -0300

    dados armazenados

--- monibank-main/js/script.js
+++ monibank-main/js/script.js
@@ -1,36 +1,52 @@
-import EhUmCPF from "./validaCPF.js";
-import EhMaiorDeIdade from "./validaIdade.js";
+import EhUmCPF from './validaCPF.js';
+import EhMaiorDeIdade from './validaIdade.js';
 
-const camposDoFormulario = document.querySelectorAll("[required]");
+const camposDoFormulario = document.querySelectorAll('[required]');
+const formulario = document.querySelector('[data-formulario]')
+
+formulario.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const listaRespostas = {
+        'nome': e.target.elements['nome'].value,
+        'email': e.target.elements['email'].value,
+        'rg': e.target.elements['rg'].value,
+        'cpf': e.target.elements['cpf'].value,
+        'aniversario': e.target.elements['aniversario'].value
+    }
+
+    localStorage.setItem('cadastro', JSON.stringify(listaRespostas));
+    window.location.href = '../pages/abrir-conta-form-2.html';
+});
 
 camposDoFormulario.forEach((campo) => {
-    campo.addEventListener("blur", () => VerificaCampo(campo));
-    campo.addEventListener("invalid", evento => evento.preventDefault());
+    campo.addEventListener('blur', () => VerificaCampo(campo));
+    campo.addEventListener('invalid', evento => evento.preventDefault());
 })
 
 const tiposDeErro = ['valueMissing', 'typeMismatch', 'patternMismatch', 'tooShort', 'customError'];
 
 const mensagens = {
     nome: {
-        valueMissing: "O campo de nome não pode estar vazio.",
-        patternMismatch: "Por favor, preencha um nome válido.",
-        tooShort: "Por favor, preencha um nome válido."
+        valueMissing: 'O campo de nome não pode estar vazio.',
+        patternMismatch: 'Por favor, preencha um nome válido.',
+        tooShort: 'Por favor, preencha um nome válido.'
     },
     email: {
-        valueMissing: "O campo de e-mail não pode estar vazio.",
-        typeMismatch: "Por favor, preencha um email válido.",
-        tooShort: "Por favor, preencha um e-mail válido."
+        valueMissing: 'O campo de e-mail não pode estar vazio.',
+        typeMismatch: 'Por favor, preencha um email válido.',
+        tooShort: 'Por favor, preencha um e-mail válido.'
     },
     rg: {
-        valueMissing: "O campo de RG não pode estar vazio.",
-        patternMismatch: "Por favor, preencha um RG válido.",
-        tooShort: "O campo de RG não tem caractéres suficientes."
+        valueMissing: 'O campo de RG não pode estar vazio.',
+        patternMismatch: 'Por favor, preencha um RG válido.',
+        tooShort: 'O campo de RG não tem caractéres suficientes.'
     },
     cpf: {
         valueMissing: 'O campo de CPF não pode estar vazio.',
-        patternMismatch: "Por favor, preencha um CPF válido.",
-        customError: "O CPF digitado não existe.",
-        tooShort: "O campo de CPF não tem caractéres suficientes."
+        patternMismatch: 'Por favor, preencha um CPF válido.',
+        customError: 'O CPF digitado não existe.',
+        tooShort: 'O campo de CPF não tem caractéres suficientes.'
     },
     aniversario: {
         valueMissing: 'O campo de data de nascimento não pode estar vazio.',
@@ -43,12 +59,12 @@ const mensagens = {
 
 
 function VerificaCampo(campo) {
-    let mensagem = "";
+    let mensagem = '';
     campo.setCustomValidity('');
-    if (campo.name == "cpf" && campo.value.length >= 11) {
+    if (campo.name == 'cpf' && campo.value.length >= 11) {
         EhUmCPF(campo);
     }
-    if (campo.name == "aniversario" && campo.value != "") {
+    if (campo.name == 'aniversario' && campo.value != '') {
         EhMaiorDeIdade(campo);
     }
     tiposDeErro.forEach(erro => {
@@ -62,7 +78,7 @@ function VerificaCampo(campo) {
     if(!validadorDeInput){
         mensagemErro.textContent = mensagem;
     }else{
-        mensagemErro.textContent = "";
+        mensagemErro.textContent = '';
     }
 }
 
